cd24f8866cb08764d462d07f5f21404e4e040b2c
Carlos De La Cruz <58544751+tabers77@users.noreply.github.com>
2020-12-11 22:26:46 +0100

    Create day7_p1.py

---
+++ day7/day7_p1.py
@@ -0,0 +1,44 @@
+f = open('input.txt', 'r')
+data = f.read().split('\n')
+f.close()
+
+bags = {}
+
+for i in range(len(data)):
+    bag, contains = data[i].split(' bags contain ')
+
+    if ', ' not in contains:
+        # If contains is equals to other bags put the bag with no values
+        if contains == 'no other bags.':
+            bags[bag] = {}
+            continue
+        else:
+            contains = [contains]
+    else:
+        contains = contains.split(', ')
+
+    content = {}
+    for k in range(len(contains)):
+        contains[k] = contains[k].split(' ')
+        content[' '.join(contains[k][1:-1])] = int(contains[k][0])
+    bags[bag] = content
+
+
+def this_in_this(looking_for, looking_in):
+    if looking_for in bags[looking_in].keys():
+        return True
+
+    for bag in bags[looking_in].keys():
+        if this_in_this(looking_for, bag):
+            return True
+
+counter = 0
+
+# if sinny gold in bags
+for i in bags.keys():
+    if this_in_this('shiny gold', i):
+        counter += 1
+
+
+if __name__ == '__main__':
+    print(counter)
