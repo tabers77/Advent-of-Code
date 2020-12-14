4585ce18315547ef92841aafafb8e36cecc471b3
Carlos De La Cruz <58544751+tabers77@users.noreply.github.com>
2020-12-14 17:33:20 +0100

    Create day9_p1.py

---
+++ day9/day9_p1.py
@@ -0,0 +1,24 @@
+with open('input.txt','r') as f:
+    data = f.readlines()
+    data = [int(i.strip()) for i in data]
+
+split = 25
+def get_preamble_num():
+    for i in range(split , len(data)): # start from the 25
+        preamble = data[i-split:i] # here we print chunks of 25
+        #print(preamble)
+        num = data[i] # all numbers staring from 25
+        found = False
+
+        for j in range(split-1): # previous 24 numbers
+            for k in range(j+1,split):
+                if preamble[j] + preamble[k] == num:
+                    found = True
+                    break
+            if found:
+                break
+        if found == True:
+            continue
+        return num
+
+print(get_preamble_num())
