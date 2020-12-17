ffb7bbb1d75d5fd14e8429358f06d5980819363c
Carlos De La Cruz <58544751+tabers77@users.noreply.github.com>
2020-12-17 18:27:48 +0100

    Update day10_p2.py

--- day10/day10_p2.py
+++ day10/day10_p2.py
@@ -1,49 +1,12 @@
-with open('input.txt') as file:
-    data = file.readlines()
-    data = [int(line.strip()) for line in data]
-    data.sort()
-
-# append the max of the last number
-data.append(max(data) + 3)
-
-def get_differ():
-    # calculate the difference
-    jolt1 = 0
-    jolt3 = 0
-
-    for i in range(len(data)-1):
-        print(data[i+1])
-        diff = data[i+1] - data[i]
-        if diff == 1:
-            jolt1 += 1
-        elif diff == 3:
-            jolt3 += 1
-
-    jolt1 = jolt1 + 1
-    ans = jolt1 * jolt3
-
-    return ans
-
-d = { }
-
-def get_combinations(pos):
-
-    if pos == len(data) - 1:
-        return 1
-
-    if pos in d.values():
-        return d[pos]
-
-    total = 0
-
-    for i in range(pos+1, len(data)):
-        if (data[i] - data[pos]) <= 3:
-            total += get_combinations(i)
-    d[pos] = total
-
-    return total
+f = open('input.txt', 'r')
+adapters = [0] + sorted(list(map(int, f.read().split('\n'))))
+f.close()
 
 
-print(get_combinations(0))
+x = [1] + [0 for _ in range(max(adapters))]
+for i in adapters:
+    for j in range(1, 3+1 ):
+        if i - j in adapters:
+            x[i] += x[i-j]
 
-
+print(x[-1])
