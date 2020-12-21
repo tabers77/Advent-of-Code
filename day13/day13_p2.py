829c9edd7fd3f1b57177b9aa54578be562b935c7
Carlos De La Cruz <58544751+tabers77@users.noreply.github.com>
2020-12-21 18:48:50 +0100

    Create day13_p2.py

---
+++ day13/day13_p2.py
@@ -0,0 +1,38 @@
+with open('input.txt') as file:
+    data = file.readlines()
+    data = [i.strip() for i in data]
+    data[1] = data[1].split(',')
+
+def modInverse(a, m):
+    a = a % m
+    for x in range(1, m):
+        if ((a * x) % m == 1):
+            return x
+    return 1
+
+def get_earlist_time():
+    ids = []
+    f_product = 1
+    for i in range(len(data[1])):
+        block = data[1][i]
+        if block != 'x':# skip the x as usual
+            k = int(block)# block as int
+            i = i % k
+            ids.append(((k-i)%k,k))
+            f_product *= k
+    total = 0
+    for i, k in ids:
+        p_product = f_product // k
+        inverse = modInverse(p_product,k)
+        assert (inverse * p_product) % k == 1
+        term = inverse * p_product * i
+        total += term
+    return total % f_product
+
+
+if __name__ == '__main__':
+    print(get_earlist_time())
+
+####Resoruces####
+#Chinese Remainder Theorem: https://en.wikipedia.org/wiki/Chinese_remainder_theorem
+#Modular Inverse: https://www.geeksforgeeks.org/multiplicative-inverse-under-modulo-m/
