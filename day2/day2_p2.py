0056c55c3b097eeb42930fd994c96ffdf9b35308
Carlos De La Cruz <58544751+tabers77@users.noreply.github.com>
2021-01-13 08:14:50 +0100

    Create day2_p2.py

---
+++ day2/day2_p2.py
@@ -0,0 +1,25 @@
+#PART 2
+import re
+
+# 1 Read the file
+with open('input.txt') as file:
+    file = file.readlines()
+    data = [char.strip('\n') for char in file]
+
+# 2  Count the valids
+valids = 0
+for i in data:
+    i = re.split('-| |:', i )
+    i.pop(3) # here I just drop
+    a = int(i[0]) - 1
+    b = int(i[1]) - 1
+    letter_to_check = i[2]
+    sample = [i for i in i[3]]
+
+    if sample[a] == letter_to_check and sample[b] != letter_to_check:
+        valids += 1
+    if sample[b] == letter_to_check and sample[a] != letter_to_check:
+        valids += 1
+
+print(valids)
+
