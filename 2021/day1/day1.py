e6f2db7c0990064aab18ba7eb3cb43662544bb11
Carlos De La Cruz <tabers77@gmail.com>
2021-12-09 16:09:21 +0100

    day 1

---
+++ 2021/day1/day1.py
@@ -0,0 +1,19 @@
+with open('day1_input') as file:
+
+    data = file.read()
+    data = [i for i in data.split()]
+
+
+# ****** code 1 ******
+count = 0
+for i in range(len(data)-1):
+    n1 = int(data[i])
+    n2 = int(data[i+1])
+    r = n2 - n1
+
+    if r > 0:
+        count += 1
+
+# ****** code 2 ******
+r = [int(data[i+1])-int(data[i]) for i in range(len(data)-1)]
+r2 = len([i for i in r if i > 0])
