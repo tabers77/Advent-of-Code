e1dc87b75fe8ee1940976f882bc728fc1057537d
Carlos De La Cruz <tabers77@gmail.com>
2021-12-13 20:06:17 +0100

    part2

--- 2021/day2/ex1.py
+++ 2021/day2/ex1.py
@@ -1,19 +1,49 @@
-with open('ex1') as file:
+def open_file(path):
 
-    data = file.readlines()
-    data = [i.split() for i in data]
+    with open(path) as file:
+        data = file.readlines()
+        data = [i.split() for i in data]
 
-horizontal = 0
-depth = 0
+    return data
 
-for tup in data:
-    if tup[0] == 'forward':
-        horizontal += int(tup[1])
-    elif tup[0] == 'down':
-        depth += int(tup[1])
-    elif tup[0] == 'up':
-        depth -= int(tup[1])
 
-answer = horizontal * depth
+def get_answer(data, exercise_n):
 
-print(answer)
+    horizontal = 0
+    depth = 0
+    aim = 0
+
+    for tup in data:
+
+        if tup[0] == 'forward':
+            horizontal += int(tup[1])
+            if aim != 0:
+                depth += int(tup[1]) * aim if exercise_n == 2 else 0
+
+        elif tup[0] == 'down':
+            depth += int(tup[1]) if exercise_n == 1 else 0
+            aim += int(tup[1])
+
+        elif tup[0] == 'up':
+            depth -= int(tup[1]) if exercise_n == 1 else 0
+            aim -= int(tup[1])
+
+    answer = horizontal * depth
+
+    return answer
+
+
+data = open_file('ex1')
+
+ans = get_answer(data=data, exercise_n=2)
+
+print(ans)
+
+
+
+
+
+
+
+
+
